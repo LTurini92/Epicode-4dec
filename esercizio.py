e3985af6ab89971eeac947727b4e7066c3920b88
LTurini92 <148910029+LTurini92@users.noreply.github.com>
2023-12-02 12:48:35 +0100

    Add files via upload

---
+++ esercizio.py
@@ -0,0 +1,47 @@
+<html>
+<head>
+<title>esercizio.py</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<style type="text/css">
+.s0 { color: #a9b7c6;}
+.s1 { color: #6a8759;}
+.s2 { color: #6897bb;}
+.s3 { color: #cc7832;}
+</style>
+</head>
+<body bgcolor="#2b2b2b">
+<table CELLSPACING=0 CELLPADDING=5 COLS=1 WIDTH="100%" BGCOLOR="#606060" >
+<tr><td><center>
+<font face="Arial, Helvetica" color="#000000">
+esercizio.py</font>
+</center></td></tr></table>
+<pre><span class="s0">input(</span><span class="s1">&quot;inserire il valore del lato&quot;</span><span class="s0">)</span>
+<span class="s0">lato=</span><span class="s2">5</span>
+<span class="s0">print(lato)</span>
+<span class="s0">input(</span><span class="s1">&quot;calcolare il perimetro del quadrato&quot;</span><span class="s0">)</span>
+<span class="s0">perimetro=lato*</span><span class="s2">4</span>
+<span class="s0">print(perimetro)</span>
+
+<span class="s3">import </span><span class="s0">math</span>
+<span class="s0">math.pi</span>
+
+<span class="s0">input(</span><span class="s1">&quot;inserire valore del raggio r&quot;</span><span class="s0">)</span>
+<span class="s0">r=</span><span class="s2">12</span>
+<span class="s0">print(r)</span>
+<span class="s0">input(</span><span class="s1">&quot;calcolare la circonferenza del cerchio&quot;</span><span class="s0">)</span>
+<span class="s0">circonferenza=</span><span class="s2">2</span><span class="s0">*math.pi*r</span>
+<span class="s0">print(circonferenza)</span>
+
+<span class="s0">input(</span><span class="s1">&quot;inserire il valore della base&quot;</span><span class="s0">)</span>
+<span class="s0">base=</span><span class="s2">16</span>
+<span class="s0">print(base)</span>
+<span class="s0">input(</span><span class="s1">&quot;inserire il valore dell'altezza&quot;</span><span class="s0">)</span>
+<span class="s0">altezza=</span><span class="s2">12</span>
+<span class="s0">print(altezza)</span>
+<span class="s0">input(</span><span class="s1">&quot;calcolare il perimetro del rettangolo&quot;</span><span class="s0">)</span>
+<span class="s0">perimetro=</span><span class="s2">2</span><span class="s0">*base+</span><span class="s2">2</span><span class="s0">*altezza</span>
+<span class="s0">print(perimetro)</span>
+
+</pre>
+</body>
+</html>
